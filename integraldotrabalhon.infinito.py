9d940ae3dd2668b272a5b15b49626be5ba9fbd34
artiemagan-08 <81157542+artiemagan-08@users.noreply.github.com>
2025-12-04 19:25:53 -0300

    Add files via upload
    Em "Notas iniciais", descrevo o processo realizado pelo programa desenvolvido em Python por mim.

---
+++ integraldotrabalhon.infinito.py
@@ -0,0 +1,16 @@
+#Escrito por ABAM para calcular a integral da função retornada por "integral(x)".
+# cd C:\Users\Mônica Ataides\OneDrive\Área de Trabalho\jogo\avulsos
+# python integral.py
+
+import mpmath
+from mpmath import mp, sqrt, quad, inf, nstr
+
+mp.dps = 80
+
+def integral(x):
+	num = 3*x**2 + d**2
+	den = (x**2 + d**2)**3
+	return mp.sqrt(mp.mpf(num)/mp.mpf(den))
+
+W = mp.quad(integral, [0, mp.inf])
+mp.nprint(W,16)
